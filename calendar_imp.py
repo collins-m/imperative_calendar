a7826482d22d8bbd303a0c5f4bb54e1a6a6866b6
Michael Collins <collins.m@pm.me>
2018-11-22 14:42:20 +0000

    Update calendar_imp.py
    if -> elif in main()

--- calendar_imp.py
+++ calendar_imp.py
@@ -8,10 +8,11 @@ calendar = {
 	"sat": [],
 }
 
-a_calendar = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]				# for checks
+a_calendar = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+                                                    # for checks
 
 def help():
-	print("-"*76)   
+	print("-"*76)
 	print("Add an appointment:")
 	print("add <day> <name of appointment> start-time <hh:mm> finish-time <hh:mm> -24hr")
 	print("-"*76)
@@ -40,23 +41,23 @@ def view_week():
 		view_day(day)
 
 def view(n):
-	if n == "calendar":													# View all
+	if n == "calendar":								# View all
 		view_week()
 
-	elif n in a_calendar:												# View day
+	elif n in a_calendar:							# View day
 		view_day(n)
 
 	else:
 		print("Input Error, type \"help\" for a list of commands")
-	
 
-def add(a):																	# Add appointment
+
+def add(a):										    # Add appointment
 	try:
 		calendar[a[1]].append([a[2], (a[3], a[4])])
 	except:
 		print("Input Error, type \"help\" for a list of commands")
 
-def remove(day, app):														# Remove appointment
+def remove(day, app):								# Remove appointment
 	try:
 		j = 0
 		for i in calendar[day]:
@@ -74,23 +75,23 @@ def main():
 			command = input().split()
 			print()
 
-			if command[0] == "exit":										# Exit loop & program
-				print("Goodbye.")
+            if command[0] == "exit":			    # Exit loop & program
+                print("Goodbye.")
 				break
 
-			if command[0] == "help":										# User manual
+            elif command[0] == "help":				# User manual
 				help()
 
-			if command[0] == "view":										# Display data
+			elif command[0] == "view":				# Display data
 				view(command[1])
 
-			if command[0] == "add":											# Add app
+			elif command[0] == "add":					# Add app
 				add(command)
 
-			if command[0] == "remove":										# Remove app
+			elif command[0] == "remove":				# Remove app
 				remove(command[1], command[2])
 
-		except:
+        except:
 			print("Input Error, type \"help\" for a list of commands")
 
 if __name__ == "__main__":
